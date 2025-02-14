da96fd95a80cdd6f050aa7e0f29c47406cf918fc
tantantan13 <tan.vananupong@gmail.com>
2025-02-13 20:33:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function sendAnswers() {
         method: "POST",
         mode: "no-cors",  // Force fetch to skip CORS check
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/text"
         },
         body: JSON.stringify({ answers })
     })
